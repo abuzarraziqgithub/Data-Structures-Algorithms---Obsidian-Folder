965f84837afd320fea410f8172f3e19a5858f0df
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-11-21 18:37:32 +0500

    vault backup: 2024-11-21 18:37:32 {{time}}
    Affected files:
    .obsidian/plugins/obsidian-icon-folder/data.json
    .obsidian/workspace.json
    Code/Big O Code/addUpTo.js.ts
    addUpTo1.js.md

--- Code/Big O Code/addUpTo.js.ts
+++ Code/Big O Code/addUpTo.js.ts
@@ -1,10 +1,33 @@
 
 /* Different Implementations for the same code: */
 
-function addUpTo1(n){
+// Pseudo Code:
+AddUp Function(10){
+    Number = 0;
+
+    Iterate 10 Times: From 0 to 10
+    {
+        0 + 1
+        1 + 2
+        2 + 3
+        3 + 4
+        4 + 5..... 10 => 
+    }
+}
+
+// Impl 1:
+function addUpTo(n){
 let total = 0;
 for (let i = 1; i <= n; i++){
     total += i;
 }
 return total;
 }
+
+
+// Impl 2:
+function addUpTo(n){
+    return n * (n + 1) / 2;
+}
+
+// Which one is better?
